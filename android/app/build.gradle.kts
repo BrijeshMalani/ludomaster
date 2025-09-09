49818510d80d79df7fbc9f2216b0a6f6a30e561f
BrijeshMalani <72848902+BrijeshMalani@users.noreply.github.com>
2025-09-09 11:22:41 +0530

    version change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 
     signingConfigs {
